a0a187cd7873e4c5329f730b44d4eff996b3fbf2
LiYongLin <33978222+EatingFoodUI@users.noreply.github.com>
2021-03-20 21:47:10 +0800

    初始化文件系统

---
+++ src/main/java/com/lyl/springboot/ossd/OssdApplication.java
@@ -0,0 +1,13 @@
+package com.lyl.springboot.ossd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OssdApplication.class, args);
+    }
+
+}
